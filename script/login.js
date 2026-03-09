613600948ce9f272841f53b796f579837a05814a
mdihbinah <mdihbinah@gmail.com>
2026-03-09 20:02:18 +0600

    b

--- script/login.js
+++ script/login.js
@@ -8,7 +8,7 @@ document.getElementById('login').addEventListener('click', function(){
     const pinNum = pinNumI.value
 
     if (mNum == 'admin' && pinNum == 'admin123'){
-        window.location.assign('../home.html')
+        window.location.assign('./home.html')
     } else{
         alert('Invalid Input....')
         return
