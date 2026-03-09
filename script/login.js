6ac595ffdc7fe0077d45ad710a49e9a3bb152c2d
mdihbinah <mdihbinah@gmail.com>
2026-03-09 09:04:56 +0600

    Dynaminc login button

---
+++ script/login.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById('login').addEventListener('click', function(){
+    const mNumI = document.getElementById('username')
+    const mNum = mNumI.value
+
+    const pinNumI = document.getElementById('pin-number')
+    const pinNum = pinNumI.value
+
+    if (mNum == 'admin' && pinNum == 'admin123'){
+        window.location.assign('http://127.0.0.1:5500/home.html')
+    } else{
+        alert('Invalid Input....')
+        return
+    }
+})
